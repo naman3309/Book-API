347094a21e19d881d95c8e14040f5b0df19c0823
Naman Goel <93463906+naman3309@users.noreply.github.com>
2023-12-26 14:59:10 +0530

    repos for same

---
+++ Repository/IAccountRepository.cs
@@ -0,0 +1,13 @@
+﻿using BookAPI.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookAPI.Repository
+{
+    public interface IAccountRepository
+    {
+        Task<IdentityResult> SignUpAsync(SignUpModel signUpModel);
+        Task<string> LoginAsync(LoginModel loginModel);
+
+    }
+
+}
