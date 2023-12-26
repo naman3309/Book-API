cda5e09c1e235b85a25883a347acccaa00f6b298
Naman Goel <93463906+naman3309@users.noreply.github.com>
2023-12-26 14:58:19 +0530

    User authentication models

---
+++ Models/SignUpModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookAPI.Models
+{
+    public class SignUpModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        
+        [Required,EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [MinLength(8, ErrorMessage = "Length of password should be atleat 8 ")]
+        //[RegularExpression("")]
+        public string Password {  get; set; }
+        [Required]
+        [Compare("Password",ErrorMessage ="Password don't match")]
+        public string ConfirmPassword {  get; set; }
+        
+    }
+}
